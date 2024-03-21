c07c3952597091a48b96d316bc9b7c33d41f87d0
callixig <callixig@gmail.com>
2024-03-21 14:38:46 +0200

    index is created

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registration page </title>
+</head>
+<body>
+    <?php
+    echo" Welcome to students MIS";
+    ?>
+    <br>
+    <a  href="registration.php" id="p"> Registration</a>
+   
+</body>
+</html>
+
+
+
